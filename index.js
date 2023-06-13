b7e4d8d06e98e7afeb67b9f00b90dca351dc9543
Danny Mendez <dmendez4424@gmail.com>
2023-06-12 22:04:47 -0700

    Added two functions that compute logic for the game

--- index.js
+++ index.js
@@ -10,3 +10,30 @@ function getComputerChoice() {
     }
     return 'Scissors';
 }
+
+function getUserChoice() {
+    let userChoice = prompt("Select a choice, Rock Paper or Scissors"); // enter user choice
+    userChoice = userChoice.toLowerCase(); // convert user input into lowercase 
+    str2 = userChoice.slice(1); // get everything after the 1st letter i.e index 0
+    userChoice = userChoice.charAt(0).toUpperCase() + str2; // get the 1st character uppercase it and and concatenate 2nd part
+    return userChoice
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Rock") return `Its A Tie! Your Choice: ${playerSelection} equals AI Choice: ${computerSelection}`;
+        else if (computerSelection === "Paper") return `You lose! Your Choice: ${playerSelection} loses to AI Choice: ${computerSelection}`;
+        return `You Win Your Choice: ${playerSelection} beats AI Choice: ${computerSelection}`; // its scissors
+    }
+    else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") return `You Win! Your Choice: ${playerSelection} beats AI Choice: ${computerSelection}`;
+        else if (computerSelection === "Paper") return `Its A Tie! Your Choice: ${playerSelection} equals AI Choice: ${computerSelection}`;
+        return `You lose! Your Choice: ${playerSelection} loses to AI Choice: ${computerSelection}`; // its scissors
+    } else {
+        // player selection must be scissors no need to check
+        if (computerSelection === "Rock") return`You lose! Your Choice: ${playerSelection} loses to AI Choice: ${computerSelection}`;
+        else if (computerSelection === "Paper") return `You Win Your Choice: ${playerSelection} beats AI Choice: ${computerSelection}`;
+        return `Its A Tie! Your Choice: ${playerSelection} equals AI Choice: ${computerSelection}`; // its scissors
+    }
+}
